f8729c370c25707d5c7064419317422ec21badaf
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-05-28 16:57:34 +0400

    navigate programatically by clicking button

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
 import Link from "next/link";
+import{useRouter} from "next/router";
 
 const Home = () => {
+
+    const router = useRouter();
+
+    const handleClick = () => {
+        console.log('Placing your order');
+        router.push('/product');
+        //or router.replace to replace the history
+        // router.replace('/product');
+    }
     return (
         <div>
             <h1>Home page</h1>
@@ -10,6 +20,9 @@ const Home = () => {
             <Link href="/product">
               <a>Products</a>
             </Link>
+            <button onClick={handleClick}>
+                Place Order
+            </button>
         </div>
     );
 };
